c6c58d858f1288dd2cf3605554ea17795ab3069d
trito apriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2022-03-03 17:02:23 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -6,7 +6,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + '/views/home.html')
 })
 
 router.get('/api', (req, res) => {
